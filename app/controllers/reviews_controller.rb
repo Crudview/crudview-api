eeed7e82e198b02dd4b47bc97ca3136155d2cf8d
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-02-08 20:32:32 -0500

    completed review and restaurant controllers, both fully rendering json. Created user and auth controllers to handle user sign up and login

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
 class ReviewsController < ApplicationController
+  before_action :set_reviews, only[:show, :update, :destroy]
+
+  def index
+    @reviews = Review.all
+    render json: @reviews
+  end
+
+  def show
+    render json: @review
+  end
+
+  def create
+    @review = Review.createt(review_params)
+  end
+
+  def update
+    @review = Review.update(review_params)
+  end
+
+  def destroy
+    @review.destroy
+  end
+
+  private
+
+  def set_reviews
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.permit(:comment, :user_id, :restaurant_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :users
+  get 'auth/login'
+  get 'auth/persist'
+  resources :reviews, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :update]
   resources :restaurants, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
