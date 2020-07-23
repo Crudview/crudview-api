cf3fcb3e668f04986ecd2a583f9c3f125f949360
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-07-23 16:23:28 -0400

    debugging auth bugs

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,8 @@
 class AuthController < ApplicationController
+  # def hmac_secret
+  #   "secret_tag"
+  # end
+
   def login
     user = User.find_by(username: login_params[:username])
     if user && user.authenticate(login_params[:password])
